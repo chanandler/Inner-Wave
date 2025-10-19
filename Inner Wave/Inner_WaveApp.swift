f59a37840302e36e53360834536a23b965756cde
Clint Yarwood <chanandler@me.com>
2025-10-19 11:56:44 +0100

    Initial Commit

---
+++ Inner Wave/Inner_WaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inner_WaveApp.swift
+//  Inner Wave
+//
+//  Created by Clint Yarwood on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Inner_WaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
